a95f4654f91240e87cfa5c98cfad190dee2bcfe2
Steven <steven@eastsidegamestudio.com>
2016-02-12 14:16:23 -0800

    Basic Scene

---
+++ Build The Cloud/Assets/Jump.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Jump : MonoBehaviour
+{
+	Rigidbody2D rigid;
+	float timer = 0.0f;
+
+	void Start()
+	{
+		rigid = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer > 2.5f)
+		{
+			timer = 0.0f;
+			rigid.AddForce(new Vector2(0.0f, 200.0f));
+		}
+	}
+}
